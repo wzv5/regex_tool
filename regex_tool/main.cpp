09361f111dafa7cf36e07d497f6d90a63d162c6e
wzv5 <wzv5@msn.com>
2021-12-19 16:37:58 +0800

    优化项目结构

--- regex_tool/main.cpp
+++ regex_tool/main.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "pch.h"
 #include "mainwindow.h"
 
 int main(int argc, char* argv[])
--- regex_tool/mainwindow.cpp
+++ regex_tool/mainwindow.cpp
@@ -1,4 +1,4 @@
-﻿#include "stdafx.h"
+﻿#include "pch.h"
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
